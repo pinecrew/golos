986a74a08f309ecebefb494de60ea0536a5dd01b
Голубев Алексей <dr.freecx@gmail.com>
2015-01-26 10:53:53 +0300

    [fix] MinGW undefined M_PI

--- src/logics.hpp
+++ src/logics.hpp
@@ -2,3 +2,8 @@
 #include <cmath>
 #include "types.hpp"
 
+// windows MinGW fix
+#ifndef M_PI
+    const double M_PI   = 3.14159265358979323846;
+    const double M_PI_2 = 1.57079632679489661923;
+#endif 
