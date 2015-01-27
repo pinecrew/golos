f02b23700ae6ff0e5a45fe8edd2cae3e634c92af
Голубев Алексей <dr.freecx@gmail.com>
2015-01-27 13:34:36 +0300

    [fix] revert M_PI_2

--- src/logics.hpp
+++ src/logics.hpp
@@ -5,6 +5,7 @@
 // windows MinGW fix
 #ifndef M_PI
     const double M_PI   = 3.14159265358979323846;
+    const double M_PI_2 = 1.57079632679489661923;
 #endif
 void cell_contour( cell c, field f, std::size_t npoints, surfPoint* contour );
 cell cell_from_point( field & f, surfPoint & p );
