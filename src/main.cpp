86160ae0ffe3dfa8a2fcb13140a28a7800d3161d
Голубев Алексей <dr.freecx@gmail.com>
2015-01-27 14:29:16 +0300

    [add] test function for draw_filled_polygon

--- src/main.cpp
+++ src/main.cpp
@@ -46,10 +46,35 @@ void game_loop( void ) {
     // insert code
 }
 
+// testing function
+void draw_star( int x0, int y0, float R, float r, float angle, int n, Uint32 color ) {
+    int * x = new int [n * 2 + 1];
+    int * y = new int [n * 2 + 1];
+    float a = 0.0f, A = 0.0f;
+
+    angle *= M_PI / 180.0f;
+    for ( int i = 0; i < n * 2; i++ ) {
+        if ( i % 2 == 0 ) {
+            A = R;
+        } else {
+            A = r / 2;
+        }
+        x[i] = x0 + round( A * cos( a * M_PI / 180.0f + angle ) );
+        y[i] = y0 - round( A * sin( a * M_PI / 180.0f + angle ) );
+        a += 180.0f / (float) n;
+    }
+    x[n * 2 + 1] = x[0];
+    y[n * 2 + 1] = y[0];
+    draw_filled_polygon( x, y, 2 * n, color );
+    delete[] x;
+    delete[] y;
+}
+
 void game_render( void ) {
     SDL_RenderClear( render );
     set_coloru( COLOR_WHITE );
     draw_sphere( { screen_width / 2, screen_height / 2 }, R, f );
+    draw_star( screen_width / 2, screen_height / 2, R, R / 1.5f, 18.0f, 5, COLOR_RED );
     set_coloru( COLOR_BLACK );
     SDL_RenderPresent( render );
 }
