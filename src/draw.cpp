ad604ffd6b8808f05fea8fbb355598b1a452ab4a
Голубев Алексей <dr.freecx@gmail.com>
2015-01-30 17:40:29 +0300

    [add] draw_aaline

--- src/draw.cpp
+++ src/draw.cpp
@@ -30,6 +30,72 @@ int set_color4u( Uint8 red, Uint8 green, Uint8 blue, Uint8 alpha ) {
     return SDL_SetRenderDrawColor( _render, red, green, blue, alpha );
 }
 
+int draw_aaline( int x1, int y1, int x2, int y2 ) {
+    Uint32 intshift, erracc, erradj, erracctmp, wgt;
+    int dx, dy, tmp, xdir, y0p1, x0pxdir, result;
+    Sint32 xx0, yy0, xx1, yy1;
+    Uint8 r, g, b, a;
+    
+    result = SDL_GetRenderDrawColor( _render, &r, &g, &b, &a );
+    xx0 = x1; yy0 = y1;
+    xx1 = x2; yy1 = y2;
+    if ( yy0 > yy1 ) {
+        tmp = yy0; yy0 = yy1;
+        yy1 = tmp; tmp = xx0;
+        xx0 = xx1; xx1 = tmp;
+    }
+    dx = xx1 - xx0;
+    dy = yy1 - yy0;
+    if ( dx == 0 || dy == 0 ) {
+        return SDL_RenderDrawLine( _render, x1, y1, x2, y2 );
+    }
+    xdir = 1;
+    if ( dx < 0 ) {
+        xdir = -1;
+        dx = -dx;
+    }
+    erracc = 0;
+    intshift = 24;
+    result |= set_color3u( r, g, b );
+    result |= SDL_RenderDrawPoint( _render, x1, y1 );
+    if ( dy > dx ) {
+        erradj = ( ( dx << 16 ) / dy ) << 16;
+        x0pxdir = xx0 + xdir;
+        while ( --dy ) {
+            erracctmp = erracc;
+            erracc += erradj;
+            if ( erracc <= erracctmp ) {
+                xx0 = x0pxdir;
+                x0pxdir += xdir;
+            }
+            yy0++;
+            wgt = ( erracc >> intshift ) & 255;
+            result |= set_color4u( r, g, b, 255 - wgt );
+            result |= SDL_RenderDrawPoint( _render, xx0, yy0 );
+            result |= set_color4u( r, g, b, wgt );
+            result |= SDL_RenderDrawPoint( _render, x0pxdir, yy0 );
+        }
+    } else {
+        erradj = ( ( dy << 16 ) / dx ) << 16;
+        y0p1 = yy0 + 1;
+        while ( --dx ) {
+            erracctmp = erracc;
+            erracc += erradj;
+            if ( erracc <= erracctmp ) {
+                yy0 = y0p1;
+                y0p1++;
+            }
+            xx0 += xdir;
+            wgt = ( erracc >> intshift ) & 255;
+            result |= set_color4u( r, g, b, 255 - wgt );
+            result |= SDL_RenderDrawPoint( _render, xx0, yy0 );
+            result |= set_color4u( r, g, b, wgt );
+            result |= SDL_RenderDrawPoint( _render, xx0, y0p1 );
+        }
+    }
+    return result;
+}
+
 void draw_path(vec3s n, SDL_Point center, std::vector<vec3s> vs) {
     // отрисовываем путь
     // пока без проверки невидимых линий
@@ -40,8 +106,7 @@ void draw_path(vec3s n, SDL_Point center, std::vector<vec3s> vs) {
         {
             SDL_Point cur = surf_to_screen( n, v, center);
             if ( pe )
-                SDL_RenderDrawLine( _render, prev.x, prev.y,
-                                             cur.x, cur.y );
+                draw_aaline( prev.x, prev.y, cur.x, cur.y );
             prev = cur;
             pe = true;
         }
