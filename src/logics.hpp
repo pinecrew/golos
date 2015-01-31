d5fd117008b25214f6288623019aa6d4b78c65fe
Голубев Алексей <dr.freecx@gmail.com>
2015-01-31 15:21:16 +0300

    [upd] M_PI* accuracy

--- src/logics.hpp
+++ src/logics.hpp
@@ -5,9 +5,9 @@
 // windows MinGW fix
 #ifdef __MINGW32__
 #ifndef M_PI
-    const double M_PI   = 3.14159265358979323846;
+    const double M_PI   = 3.14159265358979323846264338327950288;
 #endif
-    const double M_PI_2 = 1.57079632679489661923;
+    const double M_PI_2 = 1.57079632679489661923132169163975144;
 #endif
 void cell_contour( cell c, field f, std::size_t npoints, vec3s* contour );
 cell cell_from_point( field & f, vec3s & p );
