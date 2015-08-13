920913090106facbb1a1695041f148ab8c829fa1
Vova Abdrakhmanov <369565@gmail.com>
2015-08-13 17:19:33 +0300

    [fix] sphere looks like real sphere

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,14 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include <iostream>
 #include "math.hpp"
 #include "window.hpp"
 #include "vectors.hpp"
 
 vec3s camera = {3,0,0}; // положение камеры в сферических координатах
-float dtheta = 0.012;
+float dtheta = 0.01;
 float dphi = 0.01;
 WindowManager window( "Game Of Life On fieldace" );
-
-vec3d F( float u, float v, float r ) {
-    return { cosf( u ) * sinf ( v ) * r, cosf( v ) * r, sinf( u ) * sinf( v ) * r };
-}
 
 void draw_quad( vec3d p0, vec3d p1, vec3d p2, vec3d p3 ) {
     glVertex3f( p0.x, p0.y, p0.z );
@@ -20,11 +17,11 @@ void draw_quad( vec3d p0, vec3d p1, vec3d p2, vec3d p3 ) {
     glVertex3f( p3.x, p3.y, p3.z );
 }
 
-void draw_sphere( int UResolution, int VResolution, float radius ) {
+void draw_sphere( float radius, int UResolution, int VResolution ) {
     const float startU = 0.0f;
     const float startV = 0.0f;
-    const float endU = M_2PI;
-    const float endV = M_PI;
+    const float endU = M_PI;
+    const float endV = M_2PI;
     const float stepU = ( endU - startU ) / (float) UResolution;
     const float stepV = ( endV - startV ) / (float) VResolution;
     glBegin( GL_QUADS );
@@ -34,11 +31,11 @@ void draw_sphere( int UResolution, int VResolution, float radius ) {
             float v = (float) j * stepV + startV;
             float un = (float) ( i + 1 ) * stepU + startU;
             float vn = (float) ( j + 1 ) * stepV + startV;
-            vec3d p0 = F( u, v, radius );
-            vec3d p1 = F( u, vn, radius );
-            vec3d p2 = F( un, v, radius );
-            vec3d p3 = F( un, vn, radius );
-            draw_quad( p0, p2, p3, p1 );
+            vec3d p0 = vec3d( radius, u, v, true );
+            vec3d p1 = vec3d( radius, u, vn, true );
+            vec3d p2 = vec3d( radius, un, vn, true );
+            vec3d p3 = vec3d( radius, un, v, true );
+            draw_quad( p0, p1, p2, p3 );
         }
     }
     glEnd();
@@ -53,7 +50,7 @@ void golos_init( void ) {
     glShadeModel( GL_SMOOTH );
     glMatrixMode( GL_PROJECTION );
     glLoadIdentity();
-    gluPerspective( 45.0f, (float) window.GetWidth() / (float) window.GetHeight(), 0.1f, 100.0f );
+    gluPerspective( 60.0f, (float) window.GetWidth() / (float) window.GetHeight(), 0.1f, 100.0f );
     glMatrixMode( GL_MODELVIEW );
 }
 
@@ -66,7 +63,28 @@ void golos_event( SDL_Event * event ) {
         case SDL_KEYDOWN:
             switch ( event->key.keysym.sym ) {
                 case SDLK_ESCAPE:
+                case SDLK_q:
                     window.KillWindow();
+                    break;
+                case SDLK_DOWN:
+                    camera.theta += dtheta;
+                    if (camera.theta > M_PI) {
+                        camera.phi += M_PI;
+                        camera.theta -= M_PI;
+                    }
+                    break;
+                case SDLK_UP:
+                    camera.theta -= dtheta;
+                    if (camera.theta < 0) {
+                        camera.phi -= M_PI;
+                        camera.theta += M_PI;
+                    }
+                    break;
+                case SDLK_RIGHT:
+                    camera.phi += dphi;
+                    break;
+                case SDLK_LEFT:
+                    camera.phi -= dphi;
                     break;
                 default:
                     break;
@@ -75,22 +93,22 @@ void golos_event( SDL_Event * event ) {
         default:
             break;
     }
+    //auto rc = vec3d(camera);
+    //std::cout << "( " << camera.theta << ", " << camera.phi << " ) -> (" <<
+    //"( " << rc.x << ", " << rc.y << ", " << rc.z << " )" << std::endl;
 }
 
 void golos_render( void ) {
     vec3d rect_camera = vec3d(camera); // положение камеры в прямоугольных координатах
-	camera.theta += dtheta;
-	camera.phi += dphi;
 
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
     glColor3f( 0.0f, 1.0f, 0.0f );
     glLoadIdentity();
-	gluLookAt( rect_camera.x, rect_camera.y, rect_camera.z, 0,  0, 0, 0, 1, 0 );
-    glRotatef( -45.0f, 1.0f, 0.0f, 0.0f );
-    glPolygonMode( GL_BACK, GL_POINT );
-    glPolygonMode( GL_FRONT, GL_LINE );
+    gluLookAt( rect_camera.x, rect_camera.y, rect_camera.z, 0,  0, 0, 0, 0, 1 );
+    glPolygonMode( GL_BACK, GL_LINE );
+    glPolygonMode( GL_FRONT, GL_POINT );
     glColor3f( 0.0f, 1.0f, 0.0f );
-    draw_sphere( 16, 16, 1.0f );
+    draw_sphere( 1.0f, 16, 32 );
     glFlush();
 }
 
