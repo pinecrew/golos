04c273b331488087ff9fec28e3ce03a8e41580d8
Vova Abdrakhmanov <369565@gmail.com>
2015-08-13 17:17:57 +0300

    [fix] M_2PI not defined in cmath

--- src/math.hpp
+++ src/math.hpp
@@ -6,8 +6,9 @@
 #ifdef __MINGW32__
 #ifndef M_PI
     const double M_PI   = 3.14159265358979323846264338327950288;
-    const double M_2PI  = M_PI * 2.0;
 #endif
 #endif
+
+const double M_2PI  = M_PI * 2.0;
 
 inline float _fmod( float x, float y ) { return fmod( fmod( x, y ) + y, y ); }
