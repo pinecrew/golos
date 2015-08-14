716e8fc823494bb0f7279557116e6851db379e8f
Alexey Golubev <dr.freecx@gmail.com>
2015-08-14 13:24:11 +0300

    [add] ttf font support

--- src/font.cpp
+++ src/font.cpp
@@ -1,96 +1,133 @@
 #include "font.hpp"
 
-const char NULL_STR = '\0';
-
-int font_load( SDL_Renderer * r, font_table_t ** t, const char * font ) {
-    unsigned int text_size = 0, abc_size = 0;
-    SDL_Texture *tex = nullptr;
-    font_table_t *a = nullptr;
-    wint_t current = 0;
-    size_t load = 1;
-    int id = 0;
-    FILE * f;
-
-    a = new font_table_t;
-    *t = a;
-    f = fopen( font, "rb" );
-    if ( f == nullptr ) {
-        return A_ERROR_OPEN_FILE;
+static inline int next_p2( int a ) {
+    int rval = 1;
+    
+    while ( rval < a ) {
+        rval <<= 1;
     }
-    fread( &( text_size ), sizeof(int), 1, f );
-    fread( &( abc_size ), sizeof(int), 1, f );
-    a->tex_name = new char [text_size];
-    a->table = new int [abc_size];
-    fread( a->tex_name, text_size, 1, f );
-    fread( &( a->t_width ), sizeof(int), 1, f );
-    fread( &( a->t_height ), sizeof(int), 1, f );
-    tex = IMG_LoadTexture( r, a->tex_name );
-    a->font = tex;
-    if ( tex == nullptr ) {
-        fclose( f );
-        return A_ERROR_LOAD_TEXTURE;
-    }
-    SDL_QueryTexture( tex, nullptr, nullptr, &( a->f_width ), &( a->f_height ) );
-    fseek( f, sizeof(int) * 4 + text_size + 1, SEEK_SET );
-    do {
-        load = fread( &current, 2, 1, f );
-        if ( current != L'\n' && current < 0xFFFF && load != 0 ) {
-            a->table[current] = id++;
-        }
-    } while ( load != 0 );
-    fclose( f );
-    return A_SUCCESS;
+    return rval;
 }
 
-void font_draw( SDL_Renderer * r, font_table_t * t, const wchar_t * text, int x, int y ) {
-    SDL_Rect wnd = { 0, 0, t->t_width, t->t_height };
-    SDL_Rect pos = { 0, 0, t->t_width, t->t_height };
-    int dy = 0, i = 0, id = 0, old_x = x;
-    wint_t current;
+void gFont::make_dlist( FT_Face face, int ch, GLuint list, GLuint * tex ) {
+    FT_BitmapGlyph bitmap_glyph;
+    int width, height, i, j;
+    FT_Bitmap bitmap;
+    FT_Glyph glyph;
+    GLubyte * data;
+    float x, y;
 
-    pos.x = x; pos.y = y;
-    while ( ( current = text[i++] ) != NULL_STR ) {
-        switch ( current ) {
-            case '\n':
-                pos.y += t->t_height;
-                pos.x = old_x;
-                continue;
-            case ' ':
-                pos.x += t->t_width;
-                continue;
-            case '\t':
-                pos.x += 4 * t->t_width;
-                continue;
-            // to upper
-            case 'a'...'z':
-            case L'а'...L'я':
-                current -= 0x20;
+    if ( FT_Load_Glyph( face, FT_Get_Char_Index( face, ch ), FT_LOAD_RENDER ) ) {
+        Panic( "FT_Load_Glyph" );
+    }
+    if ( FT_Get_Glyph( face->glyph, &glyph ) ) {
+        Panic( "FT_Get_Glyph" );
+    }
+    FT_Glyph_To_Bitmap( &glyph, ft_render_mode_normal, 0, 1 );
+    bitmap_glyph = (FT_BitmapGlyph) glyph;
+    bitmap = bitmap_glyph->bitmap;
+    width = next_p2( bitmap.width );
+    height = next_p2( bitmap.rows );
+    data = new GLubyte [ 2 * width * height ];
+    for ( j = 0; j < height; j++) {
+        for ( i = 0; i < width; i++ ) {
+            data[2 * (i + j * width)] = 255;
+            data[2 * (i + j * width) + 1] = (i >= bitmap.width || j >= bitmap.rows) ? 
+                                            0 : bitmap.buffer[i + bitmap.width * j];
+        }
+    }
+    glBindTexture( GL_TEXTURE_2D, tex[ch] );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, data );
+    delete[] data;
+    glNewList( list + ch, GL_COMPILE );
+    glBindTexture( GL_TEXTURE_2D, tex[ch] );
+    glTranslatef( bitmap_glyph->left, 0, 0 );
+    glPushMatrix();
+    glTranslatef( 0, bitmap_glyph->top-bitmap.rows, 0 );
+    x = (float) bitmap.width / (float) width;
+    y = (float) bitmap.rows / (float) height;
+    glBegin( GL_QUADS );
+        glTexCoord2d( 0, 0 ); glVertex2f( 0, bitmap.rows );
+        glTexCoord2d( 0, y ); glVertex2f( 0, 0 );
+        glTexCoord2d( x, y ); glVertex2f( bitmap.width, 0 );
+        glTexCoord2d( x, 0 ); glVertex2f( bitmap.width, bitmap.rows );
+    glEnd();
+    glPopMatrix();
+    glTranslatef( face->glyph->advance.x >> 6, 0, 0 );
+    glEndList();
+    FT_Done_Glyph( glyph );
+}
+
+gFont::gFont( std::string fontname, uint16_t height ) {
+    FT_Library lib;
+    FT_Face face;
+
+    tex = new GLuint[ UI_FONT_LIST ];
+    if ( FT_Init_FreeType( &lib ) ) {
+        Panic( "FT_Init_FreeType" );
+    }
+    if ( FT_New_Face( lib, fontname.c_str(), 0, &face ) ) {
+        Panic( "FT_New_Face exits" );
+    }
+    FT_Set_Char_Size( face, height << 6, height << 6, 96, 96 );
+    FT_Select_Charmap( face, FT_ENCODING_UNICODE );
+    list = glGenLists( UI_FONT_LIST );
+    size_t char_table_size = strlen( char_table );
+    glGenTextures( char_table_size, tex );
+    for ( size_t i = 0; i < char_table_size; i++ ) {
+        make_dlist( face, char_table[i], list, tex );
+    }
+    FT_Done_Face( face );
+    FT_Done_FreeType( lib );
+}
+
+void gFont::draw( float x, float y, const char * fmt, ... ) {
+    int size = strlen( fmt ) * 2 + 50;
+    float modelview_matrix[16];
+    std::string text;
+    va_list ap;
+
+    if ( fmt != nullptr ) {
+        while ( true ) {
+            text.resize( size );
+            va_start( ap, fmt );
+            int n = vsnprintf( (char *) text.data(), size, fmt, ap );
+            va_end( ap );
+            if ( n > -1 && n < size ) {
+                text.resize( n );
                 break;
+            }
+            if ( n > -1 ) {
+                size = n + 1;
+            } else {
+                size *= 2;
+            }
         }
-        id = t->table[current];
-        while ( id * t->t_width >= t->f_width ) {
-            id -= t->f_width / t->t_width;
-            dy++;
-        }
-        wnd.x = id * t->t_width; wnd.y = dy * t->t_height;
-        pos.x += t->t_width;
-        SDL_RenderCopy( r, t->font, &wnd, &pos );
-        dy = 0;
     }
+    glPushAttrib( GL_LIST_BIT | GL_CURRENT_BIT | GL_ENABLE_BIT | GL_TRANSFORM_BIT );
+    glMatrixMode( GL_MODELVIEW );
+    glDisable( GL_LIGHTING ); 
+    glEnable( GL_TEXTURE_2D );
+    glDisable( GL_DEPTH_TEST ); 
+    glEnable( GL_BLEND );
+    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+    glListBase( list );
+    glGetFloatv( GL_MODELVIEW_MATRIX, modelview_matrix );
+    // для вывода нескольких строк
+    glPushMatrix();
+    glLoadIdentity();
+    glMultMatrixf( modelview_matrix );
+    glTranslatef( x, y, 0 );
+    glRotatef( 180, 1.0f, 0.0f, 0.0f );
+    glCallLists( size, GL_UNSIGNED_SHORT, text.c_str() );
+    glPopMatrix();
+    /* для вывода нескольких строк */
+    glPopAttrib();
 }
 
-void font_reload( SDL_Renderer * r, font_table_t * t ) {
-    SDL_DestroyTexture( t->font );
-    t->font = IMG_LoadTexture( r, t->tex_name );
-}
-
-void font_destroy( font_table_t * t ) {
-    SDL_DestroyTexture( t->font );
-    delete[] t->tex_name;
-    delete[] t->table;
-    delete t;
-}
-
-void font_coloru( font_table_t * t, Uint32 color ) {
-    SDL_SetTextureColorMod( t->font, color >> 16, ( color >> 8 ) & 0xFF, color & 0xFF );
+gFont::~gFont() {
+    glDeleteTextures( UI_FONT_LIST, tex );
+    delete[] tex;
 }
--- src/font.hpp
+++ src/font.hpp
@@ -1,40 +1,32 @@
 #pragma once
+#ifdef __WIN32__
+    #include <windows.h>
+#elif __linux__
+    #include <GL/glx.h>
+#endif
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <ft2build.h> 
+#include FT_FREETYPE_H
+#include <ftglyph.h>
+#include <ftoutln.h>
+#include <fttrigon.h>
+#include <cstdarg>
 #include <cstdio>
-#include <cstring>
-#include <wchar.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+#include <string>
+#include <cstdint>
+#include "window.hpp"
 
-enum font_error {
-    A_NULL_OBJECT = 0,
-    A_SUCCESS,
-    A_ERROR_OPEN_FILE,
-    A_ERROR_LOAD_TEXTURE,
+class gFont {
+public:
+    gFont( std::string ttf_file, uint16_t height );
+    void draw( float x, float y, const char * fmt, ... );
+    ~gFont();
+private:
+    const uint16_t UI_FONT_LIST = 256;
+    void make_dlist( FT_Face face, int ch, GLuint list, GLuint * tex );
+    const char * char_table = " !\"#$%&'()*+,-./0123456789:;<=>?@[]\\^_`{}|~"
+                              "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    GLuint * tex;
+    GLuint list;
 };
-
-struct font_table {
-    int f_height;
-    int f_width;
-    int t_height;
-    int t_width;
-    int * table;
-    char * tex_name;
-    SDL_Texture * font;
-};
-typedef struct font_table font_table_t;
-
-int font_load( SDL_Renderer * r, font_table_t ** t, const char * font );
-void font_draw( SDL_Renderer * r, font_table_t * t, const wchar_t * text, int x, int y );
-void font_reload( SDL_Renderer * r, font_table_t * t );
-void font_destroy( font_table_t * t );
-void font_coloru( font_table_t * t, Uint32 color );
-
-// структура файла конфигурации
-// заголовок:
-//     unsigned int size;
-//     unsigned int abc_size;
-//     char * filename[size];
-//     int width;
-//     int height;
-// размер заголовка = sizeof(int) * 4 + size + 1;
-// далее алфавит до конца файла в формате UTF-8
